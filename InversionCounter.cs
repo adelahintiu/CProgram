261b93b37c2ff95704762b9e880f136f3b92cfa7
adelahintiu <adela.hintiu@gmail.com>
2016-07-03 23:48:05 +0300

    C# Program for counting inversions

---
+++ InversionCounter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    public class InversionCounter
+    {
+        int[] array;
+
+        public InversionCounter(IEnumerable<int> list)
+        {
+            array = list.ToArray();
+        }
+
+        private int Merge(int firstStartIndex, int firstEndIndex,
+                          int secondStartIndex, int secondEndIndex)
+        {
+            int counter = 0;
+            for (int i = firstStartIndex; i <= firstEndIndex; i++)
+            {
+                for (int j = secondStartIndex; j <= secondEndIndex; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        private int CountInversion(int start, int end)
+        {
+            if (start >= end)
+            {
+                return 0;
+            }
+
+            int mid = start + (end - start) / 2;
+
+            return CountInversion(start, mid) + CountInversion(mid + 1, end)
+                + Merge(start, mid, mid + 1, end);
+        }
+
+        public int GetInversion()
+        {
+            return CountInversion(0, array.Length - 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>{ 1, 5, 2, 6, 3, 9, 10, 11, 0 };
+            InversionCounter inv = new InversionCounter(list);
+            int nr;
+            nr = inv.GetInversion();
+            Console.WriteLine(nr);
+        }
+    }
+}
